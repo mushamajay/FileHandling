c8e65303003115ede876947bc83b4ddc3958056c
mushamajay <mushamajay6@gmail.com>
2017-06-12 10:18:44 +0530

    Create s11.py

---
+++ s11.py
@@ -0,0 +1,19 @@
+# Question 11 :Write a Python program to write a list to a file
+
+fruit = ['apple', 'mango', 'banana', 'watermelon', 'papaya', 'lichi']   
+ with open('text.txt', "w") as file:   
+        for f in fruit:   
+                 file.write("%s\n" % f)   
+    
+content = open('text.txt')   
+print(content.read())   
+
+'''
+output:
+[root@demo FileHandlingAssignment]# python program11.py 
+apple
+mango
+banana
+watermelon
+papaya
+lichi
