321f0a6e3a65204e066070427c4a33177687a539
mushamajay <mushamajay6@gmail.com>
2017-06-12 10:06:57 +0530

    Create s3.py

---
+++ s3.py
@@ -0,0 +1,17 @@
+file=open("filetest11.txt","w+") 
+
+file.write("Hello potti\n") 
+file.write("hello Ajay\n") 
+file.write("Musham\n")
+file.write("hello patil\n") 
+file.write("patil\n")
+file.write("skanjaj\n") 
+file.write("sahshha\n")
+file.write("sdfsfsdfsffdA\n") 
+file.write("bhjasak\n")
+file.write("username\n") 
+file.write("mushamajay   \n") 
+
+with open("filetest11","a") as myfile:
+  myfile.write("This line is an appending line")
+print myfile
