54a17c123932d9074166bf916da84e4e8ae02f86
mushamajay <mushamajay6@gmail.com>
2017-06-12 10:29:42 +0530

    Create s16.py

---
+++ s16.py
@@ -0,0 +1,22 @@
+file=open("filetest11.txt","w+") 
+
+file.write("Hello potti\n") 
+file.write("hello Ajay\n") 
+file.write("Musham\n")
+file.write("hello patil\n") 
+file.write("patil\n")
+file.write("skanjaj\n") 
+file.write("sahshha\n")
+file.write("sdfsfsdfsffdA\n") 
+file.write("bhjasak\n")
+file.write("username\n") 
+file.write("mushamajay   \n") 
+
+ 
+ 
+
+def remove_newline(fname):  
+    list = open(fname).readlines()  
+    return [s.rstrip('\n') for s in list]  
+  
+print(remove_newline("filetest11.txt"))  
