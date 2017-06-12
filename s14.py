3e7a55d6bcd57640b978f8face16a03ce892587d
mushamajay <mushamajay6@gmail.com>
2017-06-12 10:25:58 +0530

    Create s14.py

---
+++ s14.py
@@ -0,0 +1,19 @@
+file=open("filetest11.txt","w+") 
+
+file.write("Hello potti\n") 
+file.write("hello Ajay\n") 
+file.write("Musham\n")
+file.write("hello patil\n") 
+file.write("patil\n")
+file.write("skanjaj\n") 
+file.write("sahshha\n")
+file.write("sdfsfsdfsffdA\n") 
+file.write("bhjasak\n")
+file.write("username\n") 
+file.write("mushamajay   \n") 
+
+ 
+ 
+import random
+lines = open("filetest11.txt").read().splitlines()
+print random.choice(lines)
