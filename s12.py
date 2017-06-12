069c2d2ea45ea2b13798d2b5d2ccb39c113fbdb6
mushamajay <mushamajay6@gmail.com>
2017-06-12 10:23:28 +0530

    Create s12.py

---
+++ s12.py
@@ -0,0 +1,8 @@
+# Question 12 : Write a Python program to copy the contents of a file to another file
+
+from shutil import copyfile
+
+import os
+
+os.system("touch abc.txt ")
+copyfile('filetest11.txt', 'abc.txt')
